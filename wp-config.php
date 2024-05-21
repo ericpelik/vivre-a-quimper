77bfac7e4af7776037db1d69459ede7c35bf5d26
ericpelik <pleyber.eric@gmail.com>
2024-05-21 16:36:00 +0200

    mise en place des extensions

--- wp-config.php
+++ wp-config.php
@@ -21,6 +21,8 @@
 
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 /** Nom de la base de données de WordPress. */
+define('WP_CACHE', true);
+define( 'WPCACHEHOME', '/Applications/MAMP/htdocs/vivre-a-quimper/wp-content/plugins/wp-super-cache/' );
 define( 'DB_NAME', 'vivre-a-quimper' );
 
 /** Utilisateur de la base de données MySQL. */
